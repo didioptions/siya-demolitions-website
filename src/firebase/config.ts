c18c962781f4114680eaf9877032d73e901712b5
options didi <didioptions@gmail.com>
2026-02-06 17:18:44 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:390158647401:web:d0edf142c0096d825d62f8",
   "apiKey": "AIzaSyDlMlHVNqp1LdOWCm1V_5DiDisxw-oqvHk",
   "authDomain": "studio-8810193800-3dd20.firebaseapp.com",
+  "storageBucket": "studio-8810193800-3dd20.appspot.com",
   "measurementId": "",
   "messagingSenderId": "390158647401"
 };
