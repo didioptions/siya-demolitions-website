21c9d0c365d782079bb5069d8db3c8aa90fb19ed
options didi <didioptions@gmail.com>
2026-03-20 16:19:36 +0000

    fix calendar placement error

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,8 +1,14 @@
-components={{
-  Chevron: ({ orientation, className, ...props }) =>
-    orientation === "left" ? (
-      <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-    ) : (
-      <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-    ),
-}}
+<DayPicker
+  showOutsideDays={showOutsideDays}
+  className={cn("p-3", className)}
+  classNames={{ ... }}
+  components={{
+    Chevron: ({ orientation, className, ...props }) =>
+      orientation === "left" ? (
+        <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+      ) : (
+        <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+      ),
+  }}
+  {...props}
+/>
