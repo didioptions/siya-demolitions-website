0d7c17d010b185a3c6fc203a0ac28a812c792032
options didi <didioptions@gmail.com>
2026-03-21 15:30:40 +0000

    final fix calendar clean version

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,37 +1,10 @@
-// fix v2
 "use client"
 
 import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
-
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
-function Calendar({
-  className,
-  classNames,
-  showOutsideDays = true,
-  ...props
-}: CalendarProps) {
-  return (
-    <DayPicker
-      showOutsideDays={showOutsideDays}
-      className={cn("p-3", className)}
-      classNames={classNames}
-      components={{
-        Chevron: ({ orientation, className, ...props }) =>
-          orientation === "left" ? (
-            <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-          ) : (
-            <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-          ),
-      }}
-      {...props}
-    />
-  )
+export function Calendar(props: CalendarProps) {
+  return <DayPicker {...props} />
 }
-
-export { Calendar }
